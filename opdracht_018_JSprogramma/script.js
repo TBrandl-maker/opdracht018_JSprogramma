b59f8609b04c1ec1f9eb21cceb029c557273f41e
TBrandl <58363852+TBrandl-maker@users.noreply.github.com>
2020-09-07 12:10:02 +0200

    Add files via upload

---
+++ opdracht_018_JSprogramma/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+// dit is een grote som. Dit zinnetje zie ik niet in de terminal
+/* let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division", division);
+ook deze laatste twee sommen zie ik niet */
